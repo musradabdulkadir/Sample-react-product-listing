5268864590a12a66bfc0eb1c1f009be0e02130e0
Musrad <musradabdul007@gmail.com>
2026-06-29 17:41:02 +0530

    Comleted bootstrap designing

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import "./css/App.css";
 import Product from "./components/Product";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [products, setProducts] = useState([
@@ -44,12 +52,56 @@ function App() {
 
   return (
     <>
-      <h1>Products</h1>
-      <div className="productsContainer">
-        {products.map((product, index) => {
-          return <Product data={product} />;
-        })}
-      </div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1>Products</h1>
+        <Row >
+          {products.map((product, index) => {
+            return (
+              <Col xs={12} sm={6} md={4} lg={3}>
+                <Product data={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </>
   );
 }
--- project/src/components/Product.jsx
+++ project/src/components/Product.jsx
@@ -1,15 +1,20 @@
 import Button from "react-bootstrap/Button";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Product(props) {
   return (
     <>
-      <div className="productItem">
-        <img className="productImage" src={props.data.image} />
-        <p className="productTitle">{props.data.title}</p>
-        <p className="productPrice">{props.data.price}</p>
-        <Button variant="warning">Add To Cart</Button>
-      </div>
+      <Card>
+        <Card.Img variant="top" src={props.data.image} className="productImg" />
+        <Card.Body>
+          <Card.Title>{props.data.title}</Card.Title>
+          <Card.Text>
+            {props.data.price}
+          </Card.Text>
+          <Button variant="primary">View Product</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 }
